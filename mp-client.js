9cb04dbac007ad5c97cbf84eebb461cd946d71f8
TeamPopplio <teampopplio@protonmail.com>
2021-08-22 03:10:22 -0700

    Minor change for smooth auth/logout

--- mp-client.js
+++ mp-client.js
@@ -188,7 +188,7 @@ module.exports = (config, package, gamemode) => {
 		}
 	}
 
-	process.on('exit', exitHandler.bind(null,{exit:true}));
+	process.on('exit', exitHandler.bind(null,{cleanup:true}));
 	process.on('SIGINT', exitHandler.bind(null, {cleanup:true}));
 	process.on('SIGUSR1', exitHandler.bind(null, {exit:true}));
 	process.on('SIGUSR2', exitHandler.bind(null, {exit:true}));
